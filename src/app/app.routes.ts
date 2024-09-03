e21b608e48448d2f527743c05d1ccfe61e1045c3
Balaji <abalaji63030@gmail.com>
2024-09-03 11:54:20 +0530

    product-detailsPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Modules/feature/home/home.component';
 import { ProductsComponent } from './Modules/feature/products/products.component';
 import { CartComponent } from './Modules/feature/cart/cart.component';
+import { ProductDetailsComponent } from './Modules/feature/product-details/product-details.component';
 
 export const routes: Routes = [
     {path : "" , component : HomeComponent} ,
     {path : ':levelOne/:levelTwo/:levelThree' , component : ProductsComponent} ,
-    {path : 'cart' , component : CartComponent} 
+    {path : 'cart' , component : CartComponent} ,
+    {path : 'product-details/:id' , component : ProductDetailsComponent}
 ];
